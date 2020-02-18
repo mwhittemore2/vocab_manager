b33c7142d98aaa250148378b626cc98ebb867aec
Max Whittemore <mwlogik@gmail.com>
2020-02-18 18:05:20 -0500

    Adding extra comments

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
+#Set up API blueprint
 api = Blueprint('api', __name__)
 
 from . import authorization, document_retrieval, errors, translation, validation, vocab_acquisition
--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 
+#Set up authentication blueprint
 auth = Blueprint('auth', __name__)
 
 from . import views
