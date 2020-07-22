ac1bfa5770ef62d84bd7e6d37d0dd17088151f84
Max Whittemore <mwlogik@gmail.com>
2020-07-22 18:20:31 -0400

    Adding webpack configuration

---
+++ app/document_viewer/document_manager/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path")
+const webpack = require("webpack")
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist/assets"),
+        filename: "bundle.js",
+        sourceMapFilename: 'bundle.map'
+    },
+    devtool: '#source-map',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [
+                    path.resolve(__dirname, "node_modules"),
+                    path.resolve(__dirname, "src/__tests__"),
+                    path.resolve(__dirname, "src/components/ui/__mocks__")
+                ],
+                loader: 'babel-loader',
+                query: {
+                    presets: ['@babel/env', '@babel/react']
+                }
+            }
+        ]
+    },
+    mode: "development"
+    /*optimization: {
+        minimize: true
+    }*/
+}
